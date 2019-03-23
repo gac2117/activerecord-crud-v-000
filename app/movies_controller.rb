fc2e870af0bee85e99fbee8495cf47d9a10ed816
Grace Choi <oiograceoio@gmail.com>
2019-03-23 21:04:43 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -46,17 +46,15 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by
-  # release date descending
   Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  Movie.find_by(title: "Awesome Flick")
+  Movie.title = "Even Awesomer Flick"
+  Movie.save 
 end
 
 def can_update_using_update_method
